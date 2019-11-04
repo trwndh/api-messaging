4a661eac7aa9d147b8967f634f0fce273fe7ee33
triwandha febian <triwandha@gmail.com>
2019-11-04 16:51:38 +0700

    Fixed db location

--- routes/messages.js
+++ routes/messages.js
@@ -6,8 +6,8 @@ var mysql = require('mysql2')
 
 // create MySQL pool
 var pool = mysql.createPool({
-    host:'mysql',
-    port:'3306',
+    host:'localhost',
+    port:'33061',
     user:'admin',
     password:'123',
     database:'db_message',
